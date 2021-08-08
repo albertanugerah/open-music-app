b57a6f6b3deb83c8df90b9189bcf7156f2fe19a1
Albert Anugerah Putra Siregar <albertanugerah@hotmail.com>
2021-08-09 01:14:10 +0700

    local storage implemented

---
+++ src/api/uploads/handler.js
@@ -0,0 +1,31 @@
+class UploadsHandler {
+  constructor(service, validator) {
+    this._service = service;
+    this._validator = validator;
+
+    this.postUploadImageHandler = this.postUploadImageHandler.bind(this);
+  }
+
+  async postUploadImageHandler(request, h) {
+    const { data } = request.payload;
+    this._validator.validateImageHeaders(data.hapi.headers);
+
+    const filename = await this._service.writeFile(data, data.hapi);
+
+    // const fileLocation = await this._service.writeFile(data, data.hapi);
+
+    const response = h.response({
+      status: 'success',
+      message: 'Gambar berhasil diunggah',
+      data: {
+        // local storage
+        pictureUrl: `http://${process.env.HOST}:${process.env.PORT}/upload/pictures/${filename}`,
+        // cloud
+        // fileLocation,
+      },
+    });
+    response.code(201);
+    return response;
+  }
+}
+module.exports = UploadsHandler;
--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const Hapi = require('@hapi/hapi');
 const Jwt = require('@hapi/jwt');
+const path = require('path');
+const Inert = require('@hapi/inert');
 
 // error handling
 const ClientError = require('./exceptions/ClientError');
@@ -44,6 +46,11 @@ const _exports = require('./api/exports');
 const ProducerService = require('./services/rabbitmq/ProducerService');
 const ExportsValidator = require('./validator/exports');
 
+// uploads
+const uploads = require('./api/uploads');
+const StorageService = require('./services/storage/StorageService');
+const UploadsValidator = require('./validator/uploads');
+
 const init = async () => {
   const songsService = new SongsService();
   const usersService = new UsersService();
@@ -51,6 +58,7 @@ const init = async () => {
   const collaborationsService = new CollaborationsService();
   const playlistsService = new PlaylistsService(collaborationsService);
   const playlistsongsService = new PlaylistsongsService();
+  const storageService = new StorageService(path.resolve(__dirname, 'api/uploads/file/pictures'));
 
   const server = Hapi.server({
     port: process.env.PORT,
@@ -66,6 +74,9 @@ const init = async () => {
   await server.register([
     {
       plugin: Jwt,
+    },
+    {
+      plugin: Inert,
     },
   ]);
 
@@ -138,6 +149,13 @@ const init = async () => {
         service: ProducerService,
         validator: ExportsValidator,
         playlistsService,
+      },
+    },
+    {
+      plugin: uploads,
+      options: {
+        service: storageService,
+        validator: UploadsValidator,
       },
     },
   ]);
